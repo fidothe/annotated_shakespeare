43ba6e406fbd2fcfc35003603140377a5665f17c
Matt Patterson <matt@reprocessed.org>
2012-06-27 19:07:27 +0200

    The base stuff for session two. HTML and CSS need work.

--- lib/annotated_shakespeare.rb
+++ lib/annotated_shakespeare.rb
@@ -7,7 +7,31 @@ class AnnotatedShakespeare < Sinatra::Base
   set :public_folder, File.expand_path('../../public', __FILE__)
 
   get '/' do
-    @title = "Index"
+    @title = "Plays of William Shakespeare"
+    @plays = {'julius' => 'Julius Caesar', 'richard' => 'Richard II'}
     erb :index
+  end
+
+  get '/plays' do
+    redirect to('/')
+  end
+
+  get '/plays/:id' do
+    plays = {'julius' => 'Julius Caesar', 'richard' => 'Richard II'}
+    @play = plays[params[:id]]
+    @title = @play
+    @url_slug = params[:id]
+    erb :play_index
+  end
+
+  get '/plays/:id/act' do
+    redirect to("/plays/#{params[:id]}")
+  end
+
+  get '/plays/:id/act/:act_number' do
+    plays = {'julius' => 'Julius Caesar', 'richard' => 'Richard II'}
+    @play = plays[params[:id]]
+    @act = params[:act_number]
+    erb :show
   end
 end

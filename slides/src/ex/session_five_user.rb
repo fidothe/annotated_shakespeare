7e7efb3b26cf74b50e6045004c67d8dfd03dff83
Lauren Wood <lauren@textuality.com>
2012-09-17 07:07:38 -0700

    moved exercises to DocBook

---
+++ slides/src/ex/session_five_user.rb
@@ -0,0 +1,23 @@
+require 'data_mapper'
+
+class User
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :name, String
+
+  has n, :credentials
+  has n, :comments
+
+  def self.from_auth_provider(provider, token)
+    credential = Credential.first(:provider => provider, :token => token)
+    return credential.user if credential
+    nil
+  end
+
+  def self.create_from_auth_provider(provider, token)
+    user = User.create!
+    credential = Credential.create!(:provider => provider, :token => token, :user => user)
+    user
+  end
+end
